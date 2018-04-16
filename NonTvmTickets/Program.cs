77d899c67f184952133f10c1dd9744ea8371107f
Adrian Sims <asims@parkeon.com>
2018-04-16 17:40:22 +0100

    Add project files.

---
+++ NonTvmTickets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NonTvmTickets
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception($"You must specify a filename for the RCS_R_T file");
+                }
+                var filename = args[0];
+                var xdoc = XDocument.Load(filename);
+                var nonTvmTickets = xdoc.Descendants("FTOT")
+                    .Select(x => new { Ftot = x.Attribute("t").Value, Channels = x.Descendants("Channel").Select(y => y.Attribute("ch").Value).ToList() });
+                foreach (var result in nonTvmTickets)
+                {
+                    Console.WriteLine($"ftot: {result.Ftot} channels: {string.Join(",", result.Channels)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add NonTvmTickets/Program.cs
+git add NonTvmTickets/App.config
+git add NonTvmTickets/Properties/AssemblyInfo.cs
+git add NonTvmTickets/NonTvmTickets.csproj
+git add NonTvmTickets.sln
+git add .gitignore
